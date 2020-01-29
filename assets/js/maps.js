98cb8f6e3d9915703b3b152b17ae3848cdd2ad06
Alan O Shea <alanosheaclash@gmail.com>
2020-01-29 14:03:36 +0000

    Added maps1 js to allow the functionality i needed from the api

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -17,33 +17,7 @@
         ['Tarbert on the Shannon Estuary', 52.586859, -9.358040, 1]
       ];
 
-      function setMarkers(map) {
-          var image = {
-            url:"https://i.ibb.co/NVrPjGJ/sm.png",
-          size: new google.maps.Size(20, 32),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(0, 32)
-        };
-       
-        var shape = {
-          coords: [1, 1, 1, 20, 18, 20, 18, 1],
-          type: 'poly'
-        };
-        for (var i = 0; i < marks.length; i++) {
-          var mark = marks[i];
-          var markers= new google.maps.Marker({
-            position: {lat: mark[1], lng: mark[2]},
-            map: map,
-            icon: image,
-            shape: shape,
-            title: mark[0],
-            zIndex: mark[3]
-          });
-    }
-    
-// ---- Taken from Codex turotials on multiple markers info windows ----
-
-    // Info window content
+         // Info window content
     var infoWindowContent = [
         ['<div class="info_content">' +
         '<h3>Brooklyn Museum</h3>' +
@@ -57,43 +31,61 @@
         '<p>The Prospect Park Zoo is a 12-acre (4.9 ha) zoo located off Flatbush Avenue on the eastern side of Prospect Park, Brooklyn, New York City.</p>' +
         '</div>']
     ];
-        
-    // Add multiple markers to map
-    var infoWindow = new google.maps.InfoWindow(), marks, i;
+
+      function setMarkers(map) {
+          var image = {
+            url:"https://i.ibb.co/NVrPjGJ/sm.png",
+            size: new google.maps.Size(20, 32),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(0, 32)
+        };
+       
+        var shape = {
+          coords: [1, 1, 1, 20, 18, 20, 18, 1],
+          type: 'poly'
+        };
+
+        var infoWindow = new google.maps.InfoWindow(), marker, i;
+
+        for (var i = 0; i < marks.length; i++) {
+          var mark = marks[i];
+          var position = new google.maps.LatLng(mark[1], mark[2]);
+          var marker = new google.maps.Marker({
+            position: position ,
+            map: map,
+            icon: image,
+            shape: shape,
+            title: mark[0],
+            zIndex: mark[3]
+          });
+    }
     
-    // Place each marker on the map  
-    // for( i = 0; i < marks.length; i++ ) {
-    //     var position = new google.maps.LatLng(marks[i][1], marks[i][2]);
-    //     bounds.extend(position);
-    //     marker = new google.maps.Marker({
-    //         position: position,
-    //         map: map,
-    //         title: markers[i][0]
-    //     });
-        
+// ---- Taken from Codex turotials on multiple markers info windows ----
+
+       
         // Add info window to marker    
-        google.maps.event.addListener(markers, 'click', (function(markers, i) {
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
                 infoWindow.setContent(infoWindowContent[i][0]);
-                infoWindow.open(map, markers);
+                infoWindow.open(map, marker);
             }
-        })(markers, i));
+        })(marker, i));
+      }
 
-        // Center the map to fit all markers on the screen
-        map.fitBounds(bounds);
+    //     // Center the map to fit all markers on the screen
+    //     map.fitBounds(bounds);
     
 
-    // Set zoom level
-    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-        this.setZoom(14);
-        google.maps.event.removeListener(boundsListener);
-    });
+    // // Set zoom level
+    // var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+    //     this.setZoom(9);
+    //     google.maps.event.removeListener(boundsListener);
+    // });
    
 
-      var markerCluster = new MarkerClusterer(map, markers,
+      var markerCluster = new MarkerClusterer(map, marks,
             {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-
-    }
+    
 // https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=hotel&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@lat: mark[1], lng: mark[2]&key=AIzaSyCSENlQwLM6btnfzoUUCsVci5D-mGi8rzI
 
 // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=mark[1],lng: mark[2]&radius=15000&type=restaurant&key=YOUR_API_KEY=AIzaSyCSENlQwLM6btnfzoUUCsVci5D-mGi8rzI
