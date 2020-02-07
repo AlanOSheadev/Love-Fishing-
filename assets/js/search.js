a6528b8d100bf58cd2ae99d9aa4f23ddce8feaf5
Alan O Shea <alanosheaclash@gmail.com>
2020-02-07 11:29:26 +0000

    Adding custom static markers to search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -9,7 +9,83 @@ var radius
           zoom: 8.5
         });
 
-        // Create the places service.
+         var marks = [
+        ['Kells Bay', 52.025557, -10.103597, 4],
+        ['Valentia Island Bridge', 51.889028, -10.365987, 5],
+        ['Inch Beach', 52.127794, -9.972862, 3],
+        ['Fenit Pier', 52.273029, -9.864129, 2],
+        ['Tarbert on the Shannon Estuary', 52.586859, -9.358040, 1]
+    ];
+
+    // // Info window content
+    // var infoWindowContent = [
+    //     ['<div class="info_content">' +
+    //         '<h3>Kells Bay</h3>' +
+    //         '<p>Beach, Pier and Rock fishing.</p>' +
+    //         '<p>The beach and pier are easily accessible but be careful on the rocks.</p>' +
+    //         '<p>Bait: Lug, mackerel,lures and sandeel.</p>' + '</div>'],
+    //     ['<div class="info_content">' +
+    //         '<h3>Valentia Island</h3>' +
+    //         '<p>Beach, Pier and Rock fishing.</p>' +
+    //         '<p>The beach and pier are easily accessible but be careful on the rocks.</p>' +
+    //         '<p>Bait: Lug, mackerel, sandeel and lures.</p>' + '</div>'],
+    //     ['<div class="info_content">' +
+    //         '<h3>Inch Beach</h3>' +
+    //         '<p>Beach fishing at its finest one of Irelands premier beach marks.</p>' +
+    //         '<p>The beach is easily accessible but be careful on the rocks, people often drive onto the beach but be mindful of the tide as is comes in quickly and has caught out some in the past.</p>' +
+    //         '<p>Bait: Lug, mackerel and sandeel</p>' + '</div>'],
+    //     ['<div class="info_content">' +
+    //         '<h3>Fenit Pier</h3>' +
+    //         '<p>Pier and Rock fishing.</p>' +
+    //         '<p>The pier is easily accessible, one can simply drive down and park on the pier, but be careful on the rocks. This is my personal favourite ;)</p>' +
+    //         '<p>Bait: Lug, mackerel/bluey, sandeel and feathers/lures</p>' + '</div>'],
+    //     ['<div class="info_content">' +
+    //         '<h3>Tarbert on The Shannon Estuary</h3>' +
+    //         '<p>Beach, Pier and Rock fishing.</p>' +
+    //         '<p>The beach and pier are easily accessible but be careful on the rocks</p>' +
+    //         '<p>Bait: Lug, mackerel and sandeel</p>' + '</div>']
+    // ];
+
+    // I created a custom image Rodfendr for these markers
+
+    // var image = {
+    //     url: "https://i.ibb.co/NVrPjGJ/sm.png",
+    //     size: new google.maps.Size(50, 75),
+    //     origin: new google.maps.Point(0, 0),
+    //     anchor: new google.maps.Point(0, 32)
+    // };
+
+    // var shape = {
+    //     coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    //     type: 'poly'
+    // };
+
+
+    // Add multiple markers to map
+    // var infoWindow = new google.maps.InfoWindow(), marker, i;
+
+    // Place each marker on the map  
+    // for (i = 0; i < marks.length; i++) {
+    //     var position = new google.maps.LatLng(marks[i][1], marks[i][2]);
+    //     bounds.extend(position);
+    //     infos = new google.maps.Marker({
+    //         position: position,
+    //         map: map,
+    //         icon: image,
+    //         shape: shape,
+    //         title: marks[i][0]
+    //     });
+
+    //     // Add info window to marker    
+    //     google.maps.event.addListener(infos, 'click', (function (infos, i) {
+    //         return function () {
+    //             infoWindow.setContent(infoWindowContent[i][0]);
+    //             infoWindow.open(map, infos);
+    //         }
+    //     })(infos, i));
+ 
+    
+       // Create the places service.
         var service = new google.maps.places.PlacesService(map);
         var radius = document.getElementById('radiusSelect').value;
         var getNextPage = null;
